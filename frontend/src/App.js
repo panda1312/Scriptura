00fbdbb7e4f25fc7ebb4bca4058f8f8ba7f7023e
panda1312 <33173122+panda1312@users.noreply.github.com>
2025-04-30 14:22:09 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import './theme.css';
+import './dark-theme.css'; // this should come after theme.css
+import './global.css';
+import './index.css';
+
 
 function App() {
   return (
