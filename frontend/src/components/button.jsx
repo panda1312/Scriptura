6519a090c489318607b9314aedb266b1e1951761
panda1312 <33173122+panda1312@users.noreply.github.com>
2025-04-30 13:11:47 +0900

    Update button.jsx

--- frontend/src/components/button.jsx
+++ frontend/src/components/button.jsx
@@ -1,1 +1,9 @@
+import './Button.css';
 
+export default function Button({ children, ...props }) {
+  return (
+    <button className="hig-button" {...props}>
+      {children}
+    </button>
+  );
+}
