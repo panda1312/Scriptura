aec58e2cabb5aeb1877abee02d22eb754cf06e4f
panda1312 <33173122+panda1312@users.noreply.github.com>
2025-04-30 14:21:45 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './theme.css';
+import './dark-theme.css'; // this should come after theme.css
+import './global.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
