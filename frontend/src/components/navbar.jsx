0d5b5c82ae0fb0671fb243a14522c4cb50b89783
panda1312 <33173122+panda1312@users.noreply.github.com>
2025-04-30 13:16:30 +0900

    Create navbar.jsx

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <nav className="hig-navbar">
+      <div className="hig-navbar-title">Scriptura</div>
+    </nav>
+  );
+}
