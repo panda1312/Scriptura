64d478a73756a94a5ca9568b81e3c4ca658667f1
panda1312 <33173122+panda1312@users.noreply.github.com>
2025-07-04 22:17:44 +0900

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import './styles/theme.css';
 import './styles/dark-theme.css'; // this should come after theme.css
 import './styles/global.css';
 import './index.css';
-import './components/Navbar.css';
+import './components/navbar.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
