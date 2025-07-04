ea15f6188ae710fe705ccfca1228f97c6ad5871a
panda1312 <33173122+panda1312@users.noreply.github.com>
2025-07-04 22:11:36 +0900

    Update index.js
    fixed imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './theme.css';
-import './dark-theme.css'; // this should come after theme.css
-import './global.css';
+import './styles/theme.css';
+import './styles/dark-theme.css'; // this should come after theme.css
+import './styles/global.css';
 import './index.css';
 import './components/Navbar.css';
 
